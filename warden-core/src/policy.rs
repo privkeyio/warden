c32cf91f724b5c73e156f9373720eed246befd83
William K. Santiago <william@privkey.io>
2026-01-17 11:24:11 -0400

    Add policy entailment checking module

--- warden-core/src/policy.rs
+++ warden-core/src/policy.rs
@@ -252,7 +252,7 @@ impl TimeCondition {
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
 #[serde(rename_all = "lowercase")]
 pub enum DayOfWeek {
     Mon,
