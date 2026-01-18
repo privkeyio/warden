c899ec44f9cef2e233d05f2e776ff4a24d982228
Kyle 🐆 <kyle@privkey.io>
2026-01-17 22:47:29 -0500

    Skip initial tick in blacklist sync to avoid redundant sync

--- warden-api/src/auth.rs
+++ warden-api/src/auth.rs
@@ -221,6 +221,7 @@ impl TokenBlacklist {
         Some(tokio::spawn(async move {
             let mut ticker = tokio::time::interval(interval);
             ticker.set_missed_tick_behavior(tokio::time::MissedTickBehavior::Skip);
+            ticker.tick().await;
 
             loop {
                 ticker.tick().await;
